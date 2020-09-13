3bf2cd9f07596c761751ac9e5f1b54a7ebc5085f
feliperod0519 <feliperod0519@hotmail.com>
2020-09-13 01:47:22 -0400

    2020-09-13 01:47

--- get_args.py
+++ get_args.py
@@ -1,11 +1,12 @@
-# /usr/bin/env python3
-# hello
 import argparse
 
-def main():
+def get_args():
     parser = argparse.ArgumentParser(description='say hello')
     parser.add_argument('-n','--name', metavar='name', default='World', help='Nameto greet')
-    args = parser.parse_args()
+    return parser.parse_args()
+
+def main():
+    args = get_args()
     print('Hello ' + args.name + '!')
 
 if __name__ == '__main__':
