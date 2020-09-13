58dc6f922727944ca3e6156b4687f0bf79f75a58
feliperod0519 <feliperod0519@hotmail.com>
2020-09-12 23:44:43 -0400

    2020-09-12 23:44

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print('Hello Baby!')
+# /usr/bin/env python3
+# hello
+import argparse
+
+parser = argparse.ArgumentParser(description='say hello')
+parser.add_argument('-n','--name', metavar='name', default='World', help='Nameto greet')
+args = parser.parse_args()
+print('Hello ' + args.name + '!')
