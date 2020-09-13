b7d765959c688fc56b691002fdfee68cbf6c4524
feliperod0519 <feliperod0519@hotmail.com>
2020-09-13 01:49:40 -0400

    2020-09-13 01:49

---
+++ strings.py
@@ -0,0 +1,40 @@
+import argparse
+
+# word = 'narwhal'
+# word[0]  --> 'n'
+# 'narwhal'[0] --> 'n'
+# word[6] --> 'l'
+# word[-1] --> 'l'
+# [start:stop] 
+# word[:3] --> 'nar'
+# word[3:] --> 'whal'
+# word.upper()
+# word.isupper() --> F
+# word.upper().isupper() --> T
+# len('narwhal') --> 7
+
+# word = 'octopus'
+# char = word[0] --> 'o'
+
+# type(char) --> <class 'str'>
+
+# char == 'a' --> F
+# char == 'o' --> T
+# word = "hola" --> nothing ; word == "hola" --> T or F
+
+# word = 'OCTOPUS'
+# char = word[0]
+# char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u' --> F
+# char = word[0].lower()
+# char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u' --> F
+
+# 'a' in 'aeiou' --> T;  'b' in 'aeiou' --> F
+
+#article = ''
+#if word[0].lower() in 'aeiou':
+#article = 'an'
+#else:
+#article = 'a'
+
+#'Ahoy, Captain, {} {} off the larboard bow!'.format(article, word)
+#f'Ahoy, Captain, {article} {word} off the larboard bow!'
